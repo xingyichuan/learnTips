d2fcd1d2726ef36e5e324f12e8dd65121404477a
xingyi <yichuan.xing@autodesk.com>
2019-01-14 13:10:36 +0800

    first change

---
+++ learn.py
@@ -0,0 +1,14 @@
+# print('The quick brown fox', 'jumps over', 'the lazy dog')
+#
+# name = input('please enter your name: ')
+# print('hello,', name)
+#
+#
+# print('''line1
+# line2
+# line3''')
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
