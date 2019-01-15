986f68e6581bf7c2deae07779324b59502bfa263
Guy-Yichuan Xing <Yichuan.Xing@autodesk.com>
2019-01-15 22:16:16 +0800

    Revert "Revert "Update learn.py""
    This reverts commit 083e00bafe1f5de8cc5712bc6437a03b38d7a790.

--- learn.py
+++ learn.py
@@ -1,14 +1,34 @@
 # print('The quick brown fox', 'jumps over', 'the lazy dog')
-#
+
 # name = input('please enter your name: ')
 # print('hello,', name)
-#
-#
+
+
 # print('''line1
 # line2
 # line3''')
 
-a = 'ABC'
-b = a
-a = 'XYZ'
-print(b)
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+
+# print('包含中文的str')
+
+# print(chr(25991))
+
+# print('\u4e2d\u6587')
+
+# print('中文'.encode('utf-8'))
+
+# print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
+
+# print(b'\xe4\xb8\xad\xff'.decode('utf-8', errors='ignore'))
+
+# print('%2d-%02d' % (3123, 1))
+# print('%.2f' % 3.1415926)
+
+L = ['Apple', 123, True]
+
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print(len(s))
