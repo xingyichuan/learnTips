a4a04870ce83c7be608034fb90ff8a4783d3898c
xingyi <yichuan.xing@autodesk.com>
2019-02-20 18:25:03 +0800

    update

--- learn_map.py
+++ learn_map.py
@@ -33,4 +33,17 @@ def char2num(s):
      digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
      return digits[s]
 
-print(reduce(fn, map(char2num, '13579')))
+
+print(list(map(char2num,'12345')))
+
+print(reduce(fn, map(char2num, '12345')))
+
+
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
